407b27e799676c49118039a469825d66f82c2aa0
Nerly Ton <nerlyton@gmail.com>
2020-08-01 06:24:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,6 +60,7 @@ holiday_supplies.each do |season, data|
     end
   end
 end
+end
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
