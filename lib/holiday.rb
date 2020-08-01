f385004b3d2cfac95b131c4fc86240d1afeb63fd
Nerly Ton <nerlyton@gmail.com>
2020-08-01 06:58:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,11 +75,11 @@ end
 #   end
 # end
 
-# def all_holidays_with_bbq(holiday_hash)
-#   # return an array of holiday names (as symbols) where supply lists
-#   # include the string "BBQ"
+def all_holidays_with_bbq(holiday_hash)
+  # return an array of holiday names (as symbols) where supply lists
+  # include the string "BBQ"
 
-# end
+end
 
 
 
