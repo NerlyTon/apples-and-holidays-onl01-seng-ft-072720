2723f3254a3e1d6d5906f83456a48a2f32b23495
Nerly Ton <nerlyton@gmail.com>
2020-08-01 06:32:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,9 +52,10 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 holiday_supplies.each do |season, data|
-  if season == winter
+    if season == winter
     data.each do |holiday, supplies|
       puts "#{holiday}: #{supplies}"
+      
   # data.each do |holiday, supplies|
   #   if holiday == :winter 
   #     supplies.each do |supply|
