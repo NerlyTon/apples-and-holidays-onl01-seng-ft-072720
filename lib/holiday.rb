b21c406031c8903306956452aa92561e06c56fc4
Nerly Ton <nerlyton@gmail.com>
2020-08-01 05:44:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,6 +28,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   
   holiday_supplies[:winter][:christmas][:new_years] << ["Supply"]
+  
+  holiday_supplies
 end
 
 
